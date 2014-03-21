46c1d327c3b0c9381ad223cf03cc82de59f497df
mlaterman <mlaterman@gmail.com>
2014-03-21 14:48:21 -0600

    ML: bulk uploaders specify passwords

--- bulk-streams-uploader.js
+++ bulk-streams-uploader.js
@@ -1,13 +1,11 @@
 var http = require('http');
 var str = JSON.stringify([
-{id:1, name:'user1', follows:[2,3,4,5], password: 'user1'},
-{id:2, name:'user2', follows:[3,4,5], password:'user2'},
-{id:3, name:'user3', follows:[4,5], password:'user3'},
-{id:4, name:'user4', follows:[5], password:'user4'},
-{id:5, name:'user5', follows:[], password:'user5'}]);
+{id:1, user_id:'1', path: './photos/1.png'},
+{id:2, user_id:'1', path: './photos/2.jpg'},
+{id:3, user_id:'2', path: './photos/1.png'}]);
 var opts = {
 	host: "node.cs.ucalgary.ca",
-	path: "/bulk/users?password=thunder",
+	path: "/bulk/streams?password=thunder",
 	port: 8500,
 	method: "POST",
 	headers:{'Content-Type' : 'application/json',
