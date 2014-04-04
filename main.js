0a9b91ed5950e3fe1b6a85bdb4debf5e28c101ed
mlaterman <mlaterman@gmail.com>
2014-04-03 18:04:42 -0600

    ML: attempted to go back to asyn stream uploads

--- main.js
+++ main.js
@@ -363,15 +363,19 @@ app.post('/bulk/users', function(req, res) {
 
 app.post('/bulk/streams', function(req, res) {
 	if(req.query.password == passwrd) {
-		var num = req.body.length;
-		for(var i = 0; i < num; i++) {
-			var id = req.body[i].id+1;  // add one to the id to stop db issues
-			var uid = req.body[i].user_id;
-			var path = req.body[i].path;
-			var ts = new Date(req.body[i].timestamp);
- 			db._photoInsert(id, uid, ts, "bulk"+id, path);
-		}
-		res.send(200, "Photos Uploaded");
+		async.each(req.body, function(body, callback) {
+			var id = body.id+1;  // add one to the id to stop db issues
+			var uid = body.user_id;
+			var path = body.path;
+			var ts = new Date(body.timestamp);
+			var ext = path.match(/\.[a-zA-Z]{1,4}$/);
+			db._photoInsert(id, uid, ts, id, path);
+			callback();
+		}, function(err) {
+			if(err)
+				util.log("ERROR WITH BULK PHOTO UPLOAD");
+			res.send(200, "Feeds Uploaded");
+		});
 	} else {
 		respond400('Incorrect Password', res);
 	}
