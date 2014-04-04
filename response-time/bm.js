650369cff240fde24d5a6c7d6bddc7468a6efe76
Marvin <FinAmethyst@gmail.com>
2014-04-03 18:24:48 -0600

    new version test file

--- response-time/bm.js
+++ response-time/bm.js
@@ -1,4 +1,9 @@
 /**
+== Version 0.1.0 ==
+- concurrent requests added
+- timing added
+- sends sid cookie while loading thumbnails
+
 == Version 0.0.4 ==
 - loads thumbnails
 
@@ -14,6 +19,13 @@ bug fixes:
 - Cookie is set correctly to "'Cookie': (sid + ';')".
 */
 
+const CMD_CLEAR_DB = 'Clear DB';
+const CMD_BULK_USERS = 'Bulk upload users';
+const CMD_BULK_PHOTOS = 'Bulk upload streams';
+const CMD_LOGIN = 'Login';
+const CMD_UPLOAD_SINGLE_PHOTO = 'Upload single photo';
+const CMD_FEED_REQUEST = 'Feed request';
+
 var request = require("request");
 var fs = require('fs');
 var path = require('path');
@@ -21,21 +33,47 @@ var http = require('http');
 var q = require('querystring');
 var rest = require('restler');
 var util = require('util');
+var async = require('async');
 
-var debug = false;
+const debug = false;
+const CONCURRENCY = 50;
 
 if (process.argv.length < 3) {
 	console.log('node ' + __filename + ' <port number> [<password>]');
 	process.exit(1);
 }
-
 var port = process.argv[2];
 var pass = process.argv.length > 3 ? process.argv[3] : false;
-//var baseUrl = 'http://node.cs.ucalgary.ca:' + port + '/';
-var baseUrl = 'http://localhost:' + port + '/';
+var host = 'node.cs.ucalgary.ca';
+var baseUrl = 'http://' + host + ':' + port + '/';
 var commands = [];
+var stats = [];
+var sid = 'sid=--sid--';
 
-console.log(arguments[1]);
+
+function now() { // current time in milliseconds
+	// var hrtime = process.hrtime();
+	// return (hrtime[0] * 1000 + hrtime[1] / 1000);
+	return new Date().getTime();
+}
+
+function StopWatch(taskName, index) {
+	this.taskName = taskName;
+	this.index = index;
+	
+	this.start = function(){ this.begin = now(); }
+	this.stop = function(){ this.end = now(); }
+	this.getDiff = function() {
+		if (typeof(this.begin) == 'undefined')
+			throw "start() not called on StopWatch: " + taskName + '[' + index + ']';
+		
+		if (typeof(this.end) == 'undefined')
+			throw "stop() not called on StopWatch: " + taskName + '[' + index + ']';
+
+		return this.end - this.begin; 
+	}
+}
+
 function getUrl() {
 	var path = arguments[0] || '';
 	var usePass = (typeof(arguments[1]) == 'undefined' ? true : arguments[1]);
@@ -47,16 +85,37 @@ function getUrl() {
 	return url;
 }
 
-function loadThumbnails(urls) {
+function findAllThumbnails(body) {
+	var re = /src\s?=\s?('|")(\/photos\/thumbnail\/[^'">]+)/gi;
+	var match;
+	var links = [];
+	while(match = re.exec(body)) {
+		links.push(match[2]);
+	}
+	
+	return links;
+}
+
+function loadThumbnails(urls, onEnd) {
 	function load(i, urls) {
-		if (i >= urls.length)
+		if (i >= urls.length) {
+			onEnd();
 			return;
+		}
 		
 		if (debug) {
 			console.log('loading', i + 1, 'of', urls.length, 'thumbnails');
 		}
 		
-		var req = http.get(getUrl(urls[i], false), function(res){
+		var options = {
+			hostname: host,
+			port: port,
+			path: urls[i],
+			mathod: 'GET',
+			headers: { 'Cookie': sid }
+		};
+		
+		var req = http.request(options, function(res){
 			res.on('data', function(data){
 				// console.log(data);
 			})
@@ -77,7 +136,9 @@ function loadThumbnails(urls) {
 		req.on('error', function(e){
 			console.log('error while loading ' + urls[i], e);
 			throw e;
-		});			
+		});
+		
+		req.end();
 	}
 	
 	load(0, urls);
@@ -87,49 +148,81 @@ function runCommand(index) {
 	var cmd = commands[index];
 	console.log('----------------');
 	console.log(index + ' running command "' + cmd.name + '"');
-	cmd.command(index, createHandlerFor(cmd.name, index, cmd.success));
+
+	var watchList = [];
+	var concurrent = (typeof(cmd.concurrent) == 'undefined') ? 1 : cmd.concurrent;
+	var running = concurrent;
+	var succ = function(response, body, subtaskIndex) {
+		watchList[subtaskIndex].stop();
+		if((typeof(cmd.success) == 'undefined' || cmd.success(response, body)) && (--running) == 0) {
+			
+			stats.push({
+				commandName: cmd.name,
+				timings: watchList.map(function(w){ return w.getDiff(); })
+			});
+			
+			if (index + 1 < commands.length) {
+				runCommand(index + 1);
+			} else {
+				printStats();
+			}
+			
+		}				
+	}
+	
+	for (var i = 0; i < concurrent; i++) {
+		var watch = new StopWatch(cmd.name, i);
+		watchList.push(watch);
+		watch.start();
+		cmd.command(createHandlerFor(cmd.name, index, succ, i));
+	}	
 }
 
-function createHandlerFor(commandName, index, success) {
+function createHandlerFor(commandName, index, success, subtaskIndex) {
 	return function(error, response, body) {
 		if (error) {
-			console.log(index + '. ' + commandName + ' error: ' + error);
+			console.log(index + '.' + subtaskIndex + '- ' + commandName + ' error: ' + error);
 			if (debug) {
-				console.log(index + '. ' + commandName + ' body: ' + body);
-			}			
+				console.log(index + '.' + subtaskIndex + '- ' + commandName + ' body: ' + body);
+			}
+			throw error;
 		} else {
-			console.log(index + '. ' + commandName + ' OK');
+			console.log(index + '.' + subtaskIndex + '- ' + commandName + ' OK');
 			if (debug) {
-				console.log(index + '. ' + commandName + ' body: ' + body);
+				console.log(index + '.' + subtaskIndex + '- ' + commandName + ' body: ' + body);
 			}
-			
-			var go_on = true;
-			if (typeof(success) != 'undefined') {
-				go_on = success(response, body);
-			}
-			
-			if (go_on && index + 1 < commands.length) {
-				runCommand(index + 1);
-			}
+			success(response, body, subtaskIndex);
 		}
 	}
 }
 
-function findAllThumbnails(body) {
-	var re = /src\s?=\s?('|")\/?(photos\/thumbnail\/[^'">]+)/gi;
-	var match;
-	var links = [];
-	while(match = re.exec(body)) {
-		links.push(match[2]);
+function printStats() {
+	if (debug) {
+		console.log('----------');
+		console.log(stats);
 	}
 	
-	return links;
+	var pad = '                                              ';
+	var n = 25;
+	console.log('----------------------------------------------------------------------');
+	console.log((pad + 'Task name').slice(-n), '\t', 'Min', '\t', 'Max', '\t', 'Average');
+	console.log('----------------------------------------------------------------------');
+	for(var i = 0; i < stats.length; i++) {
+		var name = (pad + stats[i].commandName).slice(-n);
+		var times = stats[i].timings;
+		
+		var min = Math.min.apply(null, times);
+		var max = Math.max.apply(null, times);
+		var avg = times.reduce(function(a, b) { return a + b; }) / times.length;
+		
+		console.log(name, '\t', min, '\t', max, '\t', avg);
+	}
 }
 
 if (pass) { // if password is give, do the bulk operations
 	commands.push({
-		name: 'clear db',
-		command: function(index, handler) {
+		name: CMD_CLEAR_DB,
+		command: function(handler) {
 			request({
 				uri : getUrl('bulk/clear'),
 				method : "GET",
@@ -138,8 +231,8 @@ if (pass) { // if password is give, do the bulk operations
 	});
 
 	commands.push({
-		name: 'bulk upload users',
-		command: function(index, handler) {
+		name: CMD_BULK_USERS,
+		command: function(handler) {
 			fs.createReadStream('users.json').pipe(request.post({
 				url : getUrl('bulk/users')
 			}, handler));
@@ -147,8 +240,8 @@ if (pass) { // if password is give, do the bulk operations
 	});
 
 	commands.push({
-		name: 'bulk upload photos',
-		command: function(index, handler) {
+		name: CMD_BULK_PHOTOS,
+		command: function(handler) {
 			fs.createReadStream('photos.json').pipe(request.post({
 				url : getUrl('bulk/streams')
 			}, handler));
@@ -156,11 +249,10 @@ if (pass) { // if password is give, do the bulk operations
 	});
 } // end of bulk operations
 
-var sid = 'sid=--sid--';
 
 commands.push({
-	name: 'login',
-	command: function(index, handler) {
+	name: CMD_LOGIN,
+	command: function(handler) {
 		request({
 			uri : getUrl('sessions/create', false),
 			method : "POST",
@@ -194,9 +286,9 @@ commands.push({
 
 
 commands.push({
-	name: 'upload single photo',
-	concurrent: 50,
-	command: function(index, handler) {
+	name: CMD_UPLOAD_SINGLE_PHOTO,
+	concurrent: CONCURRENCY,
+	command: function(handler) {
 		var filePath = path.join(__dirname, '2.jpg');
 		var stats = fs.statSync(filePath);
 		var size = stats['size'];
@@ -222,9 +314,9 @@ commands.push({
 
 
 commands.push({
-	name: 'feed request',
-	concurrent: 50,
-	command: function(index, handler) {
+	name: CMD_FEED_REQUEST,
+	concurrent: CONCURRENCY,
+	command: function(handler) {
 		var options = {
 			headers: {
 				'Cookie': (sid + ';')
@@ -234,10 +326,11 @@ commands.push({
 		
 		rest.get(getUrl('feed', false), options).on('complete', function(data, response) {
 			var err = (response.statusCode >= 200 && response.statusCode < 310) ? false : 'status_code = ' + response.statusCode;
-			handler(err, response, data);
 			var thumbnails = findAllThumbnails(data);
-			console.log('Thumbnails (' + thumbnails.length + '): \n', thumbnails);
-			loadThumbnails(thumbnails);
+			if (debug) {
+				console.log('Thumbnails (' + thumbnails.length + '): \n', thumbnails);
+			}
+			loadThumbnails(thumbnails, function(){ handler(err, response, data); });
 		});
 	}
 });
